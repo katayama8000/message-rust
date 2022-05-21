72428b530216125b0e6946e3b176f85b099bc20d
katayama <tattu.0310@gmail.com>
2022-05-21 21:19:02 +0900

    ⚡️ ts

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+"use strict";
 const makeShortString = (content, maxContentLength) => {
-  if (content.length > maxContentLength) {
-    return content.substring(0, maxContentLength) + "...";
-  }
-  return content;
+    if (typeof content !== 'string') {
+        throw new Error('content must be a string');
+    }
+    if (typeof maxContentLength !== 'number') {
+        throw new Error('maxContentLength must be a number');
+    }
+    if (content.length > maxContentLength) {
+        return content.substring(0, maxContentLength) + "...";
+    }
+    return content;
 };
-
 module.exports = makeShortString;
---
+++ index.ts
@@ -0,0 +1,14 @@
+const makeShortString = (content: string, maxContentLength: number) => {
+  if (typeof content !== 'string') {
+    throw new Error('content must be a string');
+  }
+  if (typeof maxContentLength !== 'number') {
+    throw new Error('maxContentLength must be a number');
+  }
+  if (content.length > maxContentLength) {
+    return content.substring(0, maxContentLength) + "...";
+  }
+  return content;
+};
+
+module.exports = makeShortString;
