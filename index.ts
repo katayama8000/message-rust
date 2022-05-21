13779d892c62e1d4056cffc2828db502d31be13c
katayama <tattu.0310@gmail.com>
2022-05-21 20:47:04 +0900

    ⚡️

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export const makeShortString = (content: string, maxContentLength: number) => {
+const makeShortString = (content: string, maxContentLength: number) => {
   if (content.length > maxContentLength) {
     return content.substring(0, maxContentLength) + "...";
   }
