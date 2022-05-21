abe10bd5291e900577cc6a9dd2cba66302b39593
katayama <tattu.0310@gmail.com>
2022-05-21 20:49:09 +0900

    ⚡️

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const makeShortString = (content: string, maxContentLength: number) => {
+const makeShortString = (content, maxContentLength) => {
   if (content.length > maxContentLength) {
     return content.substring(0, maxContentLength) + "...";
   }
   return content;
 };
+
+console.log(makeShortString("Hello aaaaWorld", 10));
 
 module.exports = makeShortString;
