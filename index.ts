d2538ebe3236c2bf577ede00996155d0f9829a54
katayama <tattu.0310@gmail.com>
2022-05-21 20:46:01 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+export const makeShortString = (content: string, maxContentLength: number) => {
+  if (content.length > maxContentLength) {
+    return content.substring(0, maxContentLength) + "...";
+  }
+  return content;
+};
+
+module.exports = makeShortString;
